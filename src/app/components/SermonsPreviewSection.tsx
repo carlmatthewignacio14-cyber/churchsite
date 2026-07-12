8371393028190db8924aebbcbdf1a7175caa345e
Rocket-User <example@example.com>
2026-07-12 04:21:04 +0000

    Manual code change

--- src/app/components/SermonsPreviewSection.tsx
+++ src/app/components/SermonsPreviewSection.tsx
@@ -23,7 +23,6 @@ const sundayServices: SundayService[] = [
     title: 'Crossing For One Broken Life | Mark 5:1-20',
     date: 'July 12, 2026',
     facebookVideoUrl: 'https://web.facebook.com/cogopmarikinaph/videos/1989221781731174/',
-    embedUrl: 'https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fweb.facebook.com%2Fcogopmarikinaph%2Fvideos%2F1989221781731174%2F&show_text=false&width=560&t=0',
     featured: true,
   },
   {
